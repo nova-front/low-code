50fa6bd3294ba1e44ddb1a0a69464c2179990177
罗智毅 <924361501@qq.com>
2024-11-04 23:56:23 +0800

    fix: rows and maxRow 不能同时设置

--- packages/mui/src/textarea.tsx
+++ packages/mui/src/textarea.tsx
@@ -3,7 +3,7 @@ import TextField, { type TextFieldProps } from "./textfield";
 interface TextareaProps extends TextFieldProps {}
 
 const Textarea = (props: TextareaProps): JSX.Element => {
-  return <TextField multiline rows={3} maxRows={10} {...props} />;
+  return <TextField multiline rows={3} {...props} />;
 };
 
 export default Textarea;
