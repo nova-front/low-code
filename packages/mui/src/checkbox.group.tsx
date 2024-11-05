75380e0b066c2ff127c822a03d103582bd4803bc
罗智毅 <924361501@qq.com>
2024-11-06 00:09:52 +0800

    feat: 优化

--- packages/mui/src/checkbox.group.tsx
+++ packages/mui/src/checkbox.group.tsx
@@ -1,6 +1,9 @@
 import { useState, useCallback } from "react";
+import FormControl from "@mui/material/FormControl";
 import FormGroup from "@mui/material/FormGroup";
+import FormLabel from "@mui/material/FormLabel";
 import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
 import CheckboxBox from "./checkbox";
 
 interface OptionProps {
@@ -12,18 +15,27 @@ interface OptionProps {
 interface CheckboxGroupProps {
   defaultValue?: (string | number)[];
   disabled?: boolean;
-  name?: string;
+  label?: string;
   options: string[] | number[] | OptionProps[];
   value?: (string | number | boolean)[];
   onChange?: (checkedValue: T[]) => void;
+  row?: boolean;
+  required?: boolean;
+  error?: boolean;
+  helperText?: React.ReactNode;
 }
 
 const CheckboxGroup = ({
   defaultValue,
   value,
+  label,
   options,
   disabled,
   onChange,
+  row,
+  required,
+  error,
+  helperText,
 }: CheckboxGroupProps): JSX.Element => {
   const [checks, setChecks] = useState<(string | number | boolean)[]>(
     value || defaultValue || []
@@ -55,43 +67,51 @@ const CheckboxGroup = ({
   );
 
   return (
-    <FormGroup>
-      {options.map((option: string | number | OptionProps, i: number) => {
-        if (typeof option === "string" || typeof option === "number") {
-          return (
-            <FormControlLabel
-              key={i}
-              control={
-                <CheckboxBox
-                  defaultChecked={checks?.includes(option)}
-                  checked={value ? checks?.includes(option) : undefined}
-                  disabled={disabled}
-                  onChange={(e, checked) => onChangeFn(e, checked, i, option)}
-                />
-              }
-              label={option}
-            />
-          );
-        } else {
-          return (
-            <FormControlLabel
-              key={i}
-              control={
-                <CheckboxBox
-                  defaultChecked={checks?.includes(option.value)}
-                  checked={value ? checks?.includes(option.value) : undefined}
-                  disabled={disabled || option.disabled}
-                  onChange={(e, checked) =>
-                    onChangeFn(e, checked, i, option.value)
-                  }
-                />
-              }
-              label={option.label}
-            />
-          );
-        }
-      })}
-    </FormGroup>
+    <FormControl
+      sx={{ "& .MuiFormHelperText-root": { margin: "3px 0 0 0" } }}
+      required={required}
+      error={error}
+    >
+      {label && <FormLabel component="legend">{label}</FormLabel>}
+      <FormGroup row={row}>
+        {options.map((option: string | number | OptionProps, i: number) => {
+          if (typeof option === "string" || typeof option === "number") {
+            return (
+              <FormControlLabel
+                key={i}
+                control={
+                  <CheckboxBox
+                    defaultChecked={checks?.includes(option)}
+                    checked={value ? checks?.includes(option) : undefined}
+                    disabled={disabled}
+                    onChange={(e, checked) => onChangeFn(e, checked, i, option)}
+                  />
+                }
+                label={option}
+              />
+            );
+          } else {
+            return (
+              <FormControlLabel
+                key={i}
+                control={
+                  <CheckboxBox
+                    defaultChecked={checks?.includes(option.value)}
+                    checked={value ? checks?.includes(option.value) : undefined}
+                    disabled={disabled || option.disabled}
+                    onChange={(e, checked) =>
+                      onChangeFn(e, checked, i, option.value)
+                    }
+                  />
+                }
+                label={option.label}
+              />
+            );
+          }
+        })}
+      </FormGroup>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
   );
 };
 
