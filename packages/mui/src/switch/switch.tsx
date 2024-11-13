cd4b088b762ee49c7edf0cd893bdaea41d4d0cdc
罗智毅 <924361501@qq.com>
2024-11-13 23:15:24 +0800

    feat: add switch

---
+++ packages/mui/src/switch/switch.tsx
@@ -0,0 +1,48 @@
+import SwitchBasic from "@mui/material/Switch";
+
+interface SwitchProps {
+  size?: "small" | "medium";
+  defaultChecked?: boolean;
+  disabled?: boolean;
+  color?:
+    | "primary"
+    | "secondary"
+    | "error"
+    | "info"
+    | "success"
+    | "warning"
+    | "default";
+  inputProps?: React.InputHTMLAttributes<HTMLInputElement>;
+  name?: string;
+  checked?: boolean;
+  onChange?: (
+    event: React.ChangeEvent<HTMLInputElement>,
+    checked: boolean
+  ) => void;
+}
+
+const Switch = ({
+  size,
+  defaultChecked,
+  disabled,
+  color,
+  inputProps,
+  name,
+  checked,
+  onChange,
+}: SwitchProps): JSX.Element => {
+  return (
+    <SwitchBasic
+      size={size}
+      defaultChecked={defaultChecked}
+      disabled={disabled}
+      color={color}
+      inputProps={inputProps}
+      name={name}
+      checked={checked}
+      onChange={onChange}
+    />
+  );
+};
+
+export default Switch;
