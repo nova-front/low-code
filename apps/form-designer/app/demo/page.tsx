e90f015da162302cfc4b0d975276011ab46443ad
罗智毅 <924361501@qq.com>
2025-01-08 00:01:30 +0800

    feat: add demo page

---
+++ apps/form-designer/app/demo/page.tsx
@@ -0,0 +1,7 @@
+import { Box } from "@mui/material";
+
+const Demo = () => {
+  return <Box>Demo</Box>;
+};
+
+export default Demo;
--- apps/form-designer/app/docs/page.tsx
+++ apps/form-designer/app/docs/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, Grid2 as Grid } from "@mui/material";
 import Demo from "@/components/demo";
+import { TextField, TextArea } from "@/components";
 
 const Docs = () => {
   return (
@@ -10,8 +11,39 @@ const Docs = () => {
         padding: "20px",
       }}
     >
-      <h1>基础组件</h1>
-      <Demo title="文本">文本组件1</Demo>
+      <h1 style={{ marginBottom: "16px" }}>基础组件</h1>
+      <Grid container spacing={2}>
+        <Grid size={4}>
+          <Demo title="单行文本">
+            <TextField label="姓名" fullWidth />
+          </Demo>
+        </Grid>
+        <Grid size={4}>
+          <Demo title="单行文本(error)">
+            <TextField label="姓名" fullWidth error helperText="error" />
+          </Demo>
+        </Grid>
+        <Grid size={4}>
+          <Demo title="单行文本(disabled)">
+            <TextField label="姓名" fullWidth disabled />
+          </Demo>
+        </Grid>
+        <Grid size={4}>
+          <Demo title="多行文本">
+            <TextArea label="备注" fullWidth />
+          </Demo>
+        </Grid>
+        <Grid size={4}>
+          <Demo title="多行文本(error)">
+            <TextArea label="备注" fullWidth error helperText="error" />
+          </Demo>
+        </Grid>
+        <Grid size={4}>
+          <Demo title="多行文本(disabled)">
+            <TextArea label="备注" fullWidth disabled />
+          </Demo>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
