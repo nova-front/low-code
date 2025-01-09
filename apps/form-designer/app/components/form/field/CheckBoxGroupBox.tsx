edf31d5cb7d3998449a91b450a86265a43f485db
罗智毅 <924361501@qq.com>
2025-01-10 00:03:18 +0800

    feat: add checkbox group

--- apps/form-designer/app/components/form/field/CheckBoxGroupBox.tsx
+++ apps/form-designer/app/components/form/field/CheckBoxGroupBox.tsx
@@ -1,11 +1,52 @@
-import { useEffect, useState } from "react";
-import { CheckBoxGroup } from "@/components";
+import { useCallback, useEffect, useState } from "react";
+import FormControl from "@mui/material/FormControl";
+import FormGroup from "@mui/material/FormGroup";
+import FormLabel from "@mui/material/FormLabel";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormHelperText from "@mui/material/FormHelperText";
+import { Checkbox } from "@/components";
+
 import { FormItemProps } from "@/components/form/type";
 
-const CheckBoxGroupBox = ({ fieldData }: { fieldData: FormItemProps }) => {
-  const { options = [], defaultValue, ...otherProps } = fieldData;
+interface OptionProps {
+  label: string;
+  value: string;
+  disabled?: boolean;
+}
 
-  const [value, setValue] = useState<any[]>([]);
+const CheckBoxGroupBox = ({ fieldData }: { fieldData: FormItemProps }) => {
+  const {
+    defaultValue,
+    label,
+    options = [],
+    disabled,
+    row,
+    required,
+    error,
+    helperText,
+  } = fieldData;
+
+  const [checks, setChecks] = useState<(string | number | boolean)[]>(
+    defaultValue || []
+  );
+
+  const onChangeFn = useCallback(
+    (
+      event: React.ChangeEvent<HTMLInputElement>,
+      checked: boolean,
+      index: number,
+      currentValue: string | number
+    ) => {
+      let newValue: (string | number | boolean)[] = [...checks];
+      if (checked) {
+        newValue.push(currentValue);
+      } else {
+        newValue = newValue.filter((_) => _ !== currentValue);
+      }
+      setChecks(newValue);
+    },
+    [checks]
+  );
 
   const lastOptions = options
     ?.map((option: any) => {
@@ -25,17 +66,55 @@ const CheckBoxGroupBox = ({ fieldData }: { fieldData: FormItemProps }) => {
     .filter(Boolean);
 
   useEffect(() => {
-    setValue(defaultValue);
+    setChecks(defaultValue);
   }, [defaultValue]);
 
   return (
-    <CheckBoxGroup
-      value={value}
-      onChange={(checkedValue) => setValue(checkedValue)}
-      {...otherProps}
-      row
-      options={lastOptions}
-    />
+    <FormControl
+      sx={{ "& .MuiFormHelperText-root": { margin: "3px 0 0 0" } }}
+      required={required}
+      error={error}
+    >
+      {label && <FormLabel component="legend">{label}</FormLabel>}
+      <FormGroup row={row}>
+        {lastOptions.map((option: string | number | OptionProps, i: number) => {
+          if (typeof option === "string" || typeof option === "number") {
+            return (
+              <FormControlLabel
+                key={i}
+                control={
+                  <Checkbox
+                    defaultChecked={checks?.includes(option)}
+                    checked={checks?.includes(option)}
+                    disabled={disabled}
+                    onChange={(e, checked) => onChangeFn(e, checked, i, option)}
+                  />
+                }
+                label={option}
+              />
+            );
+          } else {
+            return (
+              <FormControlLabel
+                key={i}
+                control={
+                  <Checkbox
+                    defaultChecked={checks?.includes(option.value)}
+                    checked={checks?.includes(option.value)}
+                    disabled={disabled || option.disabled}
+                    onChange={(e, checked) =>
+                      onChangeFn(e, checked, i, option.value)
+                    }
+                  />
+                }
+                label={option.label}
+              />
+            );
+          }
+        })}
+      </FormGroup>
+      {helperText && <FormHelperText>{helperText}</FormHelperText>}
+    </FormControl>
   );
 };
 
