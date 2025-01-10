14d46e3a0de3266576b7860b89b43f52a9628389
罗智毅 <924361501@qq.com>
2025-01-11 00:04:42 +0800

    chore: delete switchesgroup

--- apps/form-designer/app/config/mui.tsx
+++ apps/form-designer/app/config/mui.tsx
@@ -3,7 +3,7 @@ import TextArea from "@repo/mui/textarea";
 import Button from "@repo/mui/button";
 import Checkbox from "@repo/mui/checkbox";
 import Radio from "@repo/mui/radio";
-import SwitchesGroup from "@repo/mui/switches.group";
+import Switch from "@repo/mui/switch";
 import Select from "@repo/mui/select";
 import Autocomplete from "@repo/mui/autocomplete";
 
@@ -13,7 +13,7 @@ export {
   TextArea,
   Checkbox,
   Radio,
-  SwitchesGroup,
+  Switch,
   Select,
   Autocomplete,
 };
