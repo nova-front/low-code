6a42077cf9d3dab4cd7a665a608681503e76bd58
罗智毅 <924361501@qq.com>
2025-04-24 00:02:28 +0800

    feat: 使用DemoBox 组件

--- apps/vite-demo/src/App.tsx
+++ apps/vite-demo/src/App.tsx
@@ -1,36 +1,49 @@
+import { useRef } from "react";
 import {
   ContentEditable,
   UndoableEditor,
   UndoableEditorHandle,
 } from "@repo/editor";
-import { useRef } from "react";
+import DemoBox from "./components/demo";
 
 function App() {
   const undoableEditorRef = useRef<UndoableEditorHandle>(null);
   return (
     <div>
-      <h2>DOM + ContentEditable 实现 TextArea 基础功能</h2>
-      <div>
-        <ContentEditable spellcheck placeholder="请输入" />
-      </div>
-      <h2>use-undo 实现撤销/重做功能，同时保持与原生编辑体验的一致性</h2>
-      <UndoableEditor spellcheck ref={undoableEditorRef} />
-      <div style={{ display: "flex", gap: "12px", marginTop: "12px" }}>
-        <button onClick={() => undoableEditorRef.current?.undo()}>
-          撤销(Ctrl + Z)
-        </button>
-        <button onClick={() => undoableEditorRef.current?.redo()}>
-          重做(Ctrl + Y)
-        </button>
-        <button
-          onClick={() => {
-            const state = undoableEditorRef.current?.getState();
-            alert(JSON.stringify(state, null, 2));
+      <DemoBox title="DOM + ContentEditable 实现 TextArea 基础功能">
+        <ContentEditable placeholder="请输入" />
+      </DemoBox>
+      <DemoBox title="24px蓝色字体, 行高 1.6, 内边距 12px, 开启 spellcheck">
+        <ContentEditable
+          style={{
+            fontSize: "24px",
+            lineHeight: 1.6,
+            color: "blue",
+            padding: "12px",
           }}
-        >
-          获取状态
-        </button>
-      </div>
+          spellcheck
+          placeholder="请输入"
+        />
+      </DemoBox>
+      <DemoBox title="use-undo 实现撤销/重做功能，同时保持与原生编辑体验的一致性">
+        <UndoableEditor spellcheck ref={undoableEditorRef} />
+        <div style={{ display: "flex", gap: "12px", marginTop: "12px" }}>
+          <button onClick={() => undoableEditorRef.current?.undo()}>
+            撤销(Ctrl + Z)
+          </button>
+          <button onClick={() => undoableEditorRef.current?.redo()}>
+            重做(Ctrl + Y)
+          </button>
+          <button
+            onClick={() => {
+              const state = undoableEditorRef.current?.getState();
+              alert(JSON.stringify(state, null, 2));
+            }}
+          >
+            获取状态
+          </button>
+        </div>
+      </DemoBox>
     </div>
   );
 }
