89af5e22d07a4d91ede17ee3419e9242c049f2e5
罗智毅 <924361501@qq.com>
2025-04-24 00:01:56 +0800

    feat: DemoBox 组件

--- apps/vite-demo/src/components/demo/index.tsx
+++ apps/vite-demo/src/components/demo/index.tsx
@@ -1,5 +1,15 @@
-const DemoBox = () => {
-  return <div>demo</div>;
+interface DemoBoxProps {
+  title: React.ReactNode;
+  children: React.ReactNode;
+}
+
+const DemoBox = ({ title, children }: DemoBoxProps) => {
+  return (
+    <div>
+      <h2>{title}</h2>
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default DemoBox;
