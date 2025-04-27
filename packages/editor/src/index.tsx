b84d39f5dfb3386cd0e5a6b510383f9d182b6574
罗智毅 <924361501@qq.com>
2025-04-27 21:45:58 +0800

    chore: 删除无效代码

--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -3,6 +3,3 @@ export { UndoableEditor } from "./components/undoable-editor";
 export type { UndoableEditorHandle } from "./components/undoable-editor";
 
 export { WasmDemo } from "./components/WasmDemo";
-
-import Demo1 from "./demo/1";
-export { Demo1 };
