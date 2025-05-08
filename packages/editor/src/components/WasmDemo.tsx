ca488247b2cf92cd5e6a8f6d35e70a7ae51576f4
罗智毅 <924361501@qq.com>
2025-05-09 00:06:12 +0800

    chore: 删除无效代码

--- packages/editor/src/components/WasmDemo.tsx
+++
@@ -1,7 +0,0 @@
-export const WasmDemo = () => {
-  const onClick = async () => {
-    // const wasm = await import("../../WasmDemo-core/pkg/WasmDemo_core");
-    // wasm.greet();
-  };
-  return <div onClick={onClick}>WasmDemo</div>;
-};
--- packages/editor/src/index.tsx
+++ packages/editor/src/index.tsx
@@ -1,5 +1,3 @@
 export { ContentEditable } from "./components/content-editable";
 export { UndoableEditor } from "./components/undoable-editor";
 export type { UndoableEditorHandle } from "./components/undoable-editor";
-
-export { WasmDemo } from "./components/WasmDemo";
