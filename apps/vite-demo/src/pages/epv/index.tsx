e91a50b429b656b1ece90df6a257fac8084a9d35
罗智毅 <924361501@qq.com>
2025-05-18 22:56:30 +0800

    style: ui

--- apps/vite-demo/src/pages/epv/index.tsx
+++ apps/vite-demo/src/pages/epv/index.tsx
@@ -4,7 +4,7 @@ import DemoBox from "../../components/demo";
 function Epv() {
   document.title = "英语单词校验";
   return (
-    <div>
+    <div style={{ padding: "0 12px" }}>
       <DemoBox title="英语单词校验（学术论文、文章校验），错误单词下方自动显示红色波浪线">
         <ContentEditable
           style={{
