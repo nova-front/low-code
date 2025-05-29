286665af218410873485ac1a0a308bb645452fd4
罗智毅 <924361501@qq.com>
2025-05-29 20:50:09 +0800

    chore: apply utils and base ui

--- apps/vite-demo/src/pages/home/index.tsx
+++ apps/vite-demo/src/pages/home/index.tsx
@@ -1,5 +1,16 @@
+import { sleep } from "@nova-fe/utils";
+import { Button } from "@nova-fe/base-ui";
 const Home = () => {
-  return <div>Home</div>;
+  const clickFn = async () => {
+    await sleep(3000);
+    console.log("33");
+  };
+  return (
+    <div onClick={clickFn}>
+      Home
+      <Button appName="hi">ihh</Button>
+    </div>
+  );
 };
 
 export default Home;
