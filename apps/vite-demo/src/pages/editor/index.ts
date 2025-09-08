7734529fcbc661efce3666b10b3a6006e906984e
罗智毅 <924361501@qq.com>
2025-09-08 23:18:32 +0800

    chore: delete

--- apps/vite-demo/src/pages/editor/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as EditorLayout } from './Layout';
-export { default as BasicEditor } from './BasicEditor';
-export { default as EPV } from './EPV';
--- packages/utils/src/time.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 延迟多少毫秒后继续执行
- * @return {Promise}
- */
-
-export function sleep(time: number): Promise<any> {
-  return new Promise((resolve) => setTimeout(resolve, time));
-}
